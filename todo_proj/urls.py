ba6a85a901c537e1191b7977104f5646f5eca0b7
zhibek13 <jibek1309.jk@gmail.com>
2022-09-27 21:29:57 +0600

    Static

--- todo_proj/urls.py
+++ todo_proj/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from django.conf import settings
 
 from todo_app.views import TodoViewSet
 
@@ -25,4 +27,4 @@ router.register('todos', TodoViewSet, basename='todos')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
